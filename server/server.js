108eb06e441f61f164c2d62a4e878a40446c4afc
Haley Boyle <haleyjboyle@gmail.com>
2017-10-18 10:54:08 -0700

    autho

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ passport.deserializeUser(function(userId, done){
 app.get('/auth', passport.authenticate('auth0'));
 
 app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: process.env.HOST + '/#/account',
+    successRedirect: process.env.REACT_APP_HOST + '/#/account',
     failureRedirect: '/auth'
 }))
 
