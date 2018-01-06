6422af754f786f295151f9259882ea354f5580f3
Haley <haleyjboyle@gmail.com>
2018-01-05 19:38:34 -0700

    edited for readability

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,8 +10,7 @@ class Cart extends Component {
             return (
                 <div  key={index} className="blah">
                     <div className="one">
-                    
-                      <img  className="cart-shirt" src={item.img} alt="" />
+                    <img  className="cart-shirt" src={item.img} alt="" />
                       <h2> {item.name}</h2>
                     </div>
                 <div className="two">
@@ -22,20 +21,10 @@ class Cart extends Component {
                  <button className="shopping-cart-button" onClick={() => this.props.removeFromShoppingCart(item.id)}>X</button>
                 </div>
                 <div className="five">{ "$" + item.price * item.qty + ".00"}</div>
-                </div>
-                // <tr key={index} className="blah">
-                //     <td className="one"> <img  className="cart-shirt" src={item.img} alt="" />  <h2> {item.name}</h2></td>
-                //     <td className="two"> <h2>{"$" + item.price + ".00"}</h2></td>
-                //     <td className="three"><input placeholder={item.qty}/>update</td>
-                //     <td className="four"><button className="shopping-cart-button" onClick={() => this.props.removeFromShoppingCart(item.id)}>X</button></td>
-                //     <td className="five">{ "$" + item.price * item.qty + ".00"}</td>
-                // </tr>
+              </div>
             )
         })
 
-     
-        // console.log("shopping",this.props.shoppingCart)
-        
         return (
             <div className="shopping-cart-container">
               
@@ -44,47 +33,27 @@ class Cart extends Component {
                 <ul className='wrapper'>
                     <li className='box1'>My Bag</li>
                     <li className='box2'>
-                    {/* <tr className="label">
-                   <th className="one1">Image</th>
-                   <th className="two1">price</th>
-                   <th className="two1">QTY</th>
-                   <th className="two1">Delete</th>
-                   <th className="two1">Total</th>
-               </tr> */}
-                    {shoppingCartDisplay[0] ? 
-                shoppingCartDisplay 
-                : <div className="go-buy-something"><h1>empty cart</h1></div>}  
-                
+   //if there is nothing in the cart page will display to go buy something
+                    {shoppingCartDisplay[0] ? shoppingCartDisplay : <div className="go-buy-something"><h1>empty cart</h1></div>}  
                     </li>
                     <li className='box3'>
-
-                        <div className="subtotal">subtotal</div>
+                    <div className="subtotal">subtotal</div>
                         <div className="total"> $
-                            
-
-                        {
-                           
-                            this.props.shoppingCart.reduce((sum, item)=>{
-                                {/* console.log(item) */}
-                               return sum + (parseInt(item.price, 10) * item.qty)
+                           {
+                          this.props.shoppingCart.reduce((sum, item)=>{
+                            return sum + (parseInt(item.price, 10) * item.qty)
                             },0)
                         }
                         </div>
                         <div className="checkout">CHECKOUT</div>
-                  
-            
-                    </li>
-                    
-                    
-                    
-                </ul>
+                  </li>
+               </ul>
                </div>
              
             </div>
         )
     }
-
-    }
+}
 
     
     function mapStateToProps(state) {
