d086f38413f92239c3af6ea045d79050aa6e1900
Haley <haleyjboyle@gmail.com>
2018-01-05 19:32:49 -0700

    added notes and deleted unused code

--- src/components/womens-tees/product.js
+++ src/components/womens-tees/product.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './womens-tees.css'
 
-
+//component for individual product
 class Product extends Component {
     constructor(props){
         super(props)
@@ -19,24 +19,26 @@ class Product extends Component {
     mouseOut() {
         this.setState({hover: false});
     }
+    
+    //then mouse hovers over image the image will change to person view
     render() {
         const {shirt} = this.props
        
-            return( <div  className='item' >
-                <Link to = {`/collections/womens-tees/products/${shirt.slug}`} >
-            <div onMouseOver={this.mouseOver} onMouseOut =      {this.mouseOut}>
-            {this.state.hover ? (<img alt="" src={shirt.img2}/>) : (<img  alt="" src={shirt.img}/>)}</div>
+            return( 
+                
+                <div  className='item' >
+                <Link to = {`/collections/womens-tees/products/${shirt.slug}`}
+                <div onMouseOver={this.mouseOver} onMouseOut = {this.mouseOut}>
+                {this.state.hover ? (<img alt="" src={shirt.img2}/>) : (<img  alt="" src={shirt.img}/>)}</div>
                 <div>{shirt.name}</div>
                 <div>{shirt.gender} {shirt.type}</div>
-                
                 </Link>
                 <div>${shirt.price}</div>
-                 </div>)
+                </div>
+            )
         
         
     }
 }
 
 export default Product;
-{/* {this.state.hover ? (<img src={shirt.img}/>) : (<img src={shirt.img2}/>)}  */}
-{/* <div><img alt='' src={shirt.img}/></div> */}
